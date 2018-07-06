f42d0a490697280e5e203f316c5fbedec06c2404
William-Barry-Aix <wiwi.barry@hotmail.fr>
2018-07-06 09:20:06 +0200

    Ajout de condition vérifiant pour que les commandes soit seulement pour les movement sur les liannes

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour {
 
     private Rigidbody2D rb2D;
+    Rigidbody2D playerAnchor;
+    Rigidbody2D mapAnchor;
     public float thrust;
 
 
@@ -13,19 +15,34 @@ public class PlayerMovment : MonoBehaviour {
     void Start () {
         rb2D = GetComponent<Rigidbody2D>();
         thrust = 20.0f; 
+        playerAnchor = GetComponent<DistanceJoint2D>().connectedBody;
+        setMapAnchor(playerAnchor.gameObject.GetComponent<DistanceJoint2D>().connectedBody);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("right"))
+        if (playerAnchor)
         {
-            rb2D.AddForce(transform.right * thrust);
+            if (mapAnchor)
+            {
+                if (playerAnchor.GetComponent<DistanceKeeper>().isFixed())
+                {
+                    if (Input.GetKey("right"))
+                    {                        
+                        rb2D.AddForce(transform.right * thrust);
+                    }
+                    else if (Input.GetKey("left"))
+                    {
+                        rb2D.AddForce(transform.right * -thrust);
+                    }
+                    else if (Input.GetKey("down"))
+                    {
+                        stopSwinging();
+                    }
+                }
+            }
         }
-        else if (Input.GetKey("left"))
-        {
-            rb2D.AddForce(transform.right * -thrust);
-        }
-        else
+    }
         {
             rb2D.AddForce(transform.up * -2*thrust);
         }
