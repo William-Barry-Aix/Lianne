ab8464a5b06d7291463c688bdef84578b877678f
William-Barry-Aix <wiwi.barry@hotmail.fr>
2018-07-06 11:16:52 +0200

    le joueur ne ralentis pas plus lorsqu'il apuis sur "bas"

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -37,7 +37,7 @@ public class PlayerMovment : MonoBehaviour {
                     }
                     else if (Input.GetKey("down"))
                     {
-                        stopSwinging();
+                        //stopSwinging();
                     }
                 }
             }
