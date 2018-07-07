03dc7cadcbf8f21fb62a2697e90e213f8b13b7e2
William-Barry-Aix <wiwi.barry@hotmail.fr>
2018-07-07 09:06:37 +0200

    création du script de gestion des noeuds

--- Assets/NodesManager.cs
+++ Assets/NodesManager.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NodesManager : MonoBehaviour {
+    public GameObject node;
+    private DistanceJoint2D mapAnchorJoint;
+    private GameObject nodes;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        mapAnchorJoint = GetComponent<DistanceJoint2D>();
+        //nodes = 
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // si pas assez de noeuds
+        //creation d'un nouveau depuis la tête
+
+        //si trop de noeuds
+        //supréssion du dernier
 	}
 }
